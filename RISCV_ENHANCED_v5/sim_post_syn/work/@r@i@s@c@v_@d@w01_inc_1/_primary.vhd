b65f78572f005c70728a26e51b8e77d80cc409e2
Gianluca <gianlucagoti1996@gmail.com>
2021-02-08 21:36:58 +0100

    delete

--- RISCV_ENHANCED_v5/sim_post_syn/work/@r@i@s@c@v_@d@w01_inc_1/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity RISCV_DW01_inc_1 is
-    port(
-        A               : in     vl_logic_vector(31 downto 0);
-        SUM             : out    vl_logic_vector(31 downto 0)
-    );
-end RISCV_DW01_inc_1;
--- RISCV_ENHANCED_v5/syn/script_RISCV_enhanced/script_power_RISCV.tcl
+++
@@ -1,5 +0,0 @@
-read_verilog -netlist ../netlist/RISCV.v
-read_saif -input ../saif/myriscv_syn.saif -instance tb_riscv/riscv_comp -unit ns -scale 1
-create_clock -name MY_CLK clk
-report_power > ./report_power_RISCV.txt
-
